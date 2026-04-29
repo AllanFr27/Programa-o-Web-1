63326e970ec74ebcd71cc6182dedf0abb31b7ef6
AllanFr27 <ribeiro.allanf@gmail.com>
2026-04-29 09:58:28 -0300

    Add files via upload

---
+++ PaginaCss/paginaJavaScript.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("Bon");
+var img = document.getElementById("img1");
+
+button.onclick = function(){
+    if(button.innerHTML == "DON´T CLICK!"){
+        button.innerHTML = "I SAID DON´T CLICK!";
+    }else if(button.innerHTML == "I SAID DON´T CLICK!"){
+        button.innerHTML = "ARE YOU SURE?";
+    }else if(button.innerHTML == "ARE YOU SURE?"){
+        button.innerHTML = "OK! I´LL LET YOU CLICK.";
+    }
+}
