4972323e9f8009a61efc55845fb2f87e322a92bf
AllanFr27 <ribeiro.allanf@gmail.com>
2026-06-23 08:38:37 -0300

    Delete Page directory

--- Page/Script/main.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".navMenu");
-
-navToggle.addEventListener("click", () => {
-    navToggle.classList.toggle("active");
-    navMenu.classList.toggle('active');
-});
